0419629ca465f8ce4022db209a7a138d80367fe1
Lean Henrique <90344163+LeanHenrique@users.noreply.github.com>
2026-04-20 10:37:41 -0300

    Update 70. Climbing Stairs.cpp

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -16,3 +16,25 @@ public:
 
     }
 };
+
+
+//second solution
+class Solution {
+public:
+    int climbStairs(int n) {
+       if(n == 1) return 1;
+
+       int first = 1;
+       int second = 2;
+       int third = 0;
+
+       for(int i = 2; i < n; i++){
+        third = first + second;
+        first = second;
+        second = third; 
+        }
+
+        return second;
+    }
+
+};
